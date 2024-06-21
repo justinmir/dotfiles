81713bf7521879e3d617b437c3154b16f64ae2c5
Justin Miron <justin.miron@reddit.com>
2024-06-21 15:03:04 -0500

    automatic sync Fri Jun 21 03:03:04 PM CDT 2024

--- nvim2/init.lua
+++ nvim2/init.lua
@@ -588,7 +588,7 @@ require("lazy").setup({
 			--  - settings (table): Override the default settings passed when initializing the server.
 			--        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
 			local servers = {
-				-- clangd = {},
+				clangd = {},
 				gopls = {},
 				-- pyright = {},
 				-- rust_analyzer = {},
@@ -907,6 +907,9 @@ require("lazy").setup({
 	},
 	{
 		"airblade/vim-rooter",
+	},
+	{
+		"rhysd/vim-clang-format",
 	},
 
 	-- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
